95ebac24a709b42ec3c1ef1578c18e37e37694ff
Caio Corrêa Chaves <ccaioc.correa@gmail.com>
2025-11-09 13:57:42 -0300

    Primeiro teste com pthreads

--- src/main.c
+++ src/main.c
@@ -1,6 +1,21 @@
+#include <stdio.h>
+#include <pthread.h>
 #include "../lib/game.h"
 
+void* foo(void* arg) {
+    printf("Hello from thread!\n");
+    printf("arg: %s\n", (char*)arg);
+    return NULL;
+}
+
 int main() {
+    pthread_t thread1;
+    pthread_create(&thread1, NULL, foo, "Thread Argument");
+
+    say_hello();
+
+    pthread_join(thread1, NULL);
+
     say_hello();
     return 0;
 }
